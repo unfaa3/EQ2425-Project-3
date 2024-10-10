26d02c75b375697f037c91d0a0fe411d3ec3e64d
jiachengxu <xujiacheng1016@hotmail.com>
2024-10-10 20:21:10 +0200

    能训练了，至少。。。

--- load_data.py
+++ load_data.py
@@ -1,56 +1,45 @@
-import pickle
 import numpy as np
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
-def load_cifar_batch(filename):
-    # Load a single batch of the CIFAR-10 dataset
-    try:
-        # Use the unpickle function to load the batch
-        batch = unpickle(filename)
-        # Extract data (images) and labels from the batch
-        data = batch[b'data']
-        labels = batch[b'labels']
-        print(f'data shape: {data.shape}, labels length: {len(labels)}')
-    except FileNotFoundError:
-        # Raise an error if the file is not found
-        raise FileNotFoundError(f"The file {filename} was not found.")
-    return data, labels
-
-def load_all_batches():
-    # Load all the batches of the CIFAR-10 dataset
-    data_list = []
-    labels_list = []
-    for i in range(1, 6):
-        # Load each of the 5 data batches
-        print(f'Loading batch {i}')
-        base_path = 'cifar-10-batches-py/'
-        data, labels = load_cifar_batch(f'{base_path}data_batch_{i}')
-        # Append the data and labels to the lists
-        data_list.append(data)
-        labels_list.append(labels)
-
-    # Concatenate all data batches into a single array
-    all_data = np.concatenate(data_list, axis=0)
-    # Concatenate all label batches into a single array
-    all_labels = np.concatenate(labels_list, axis=0)
-    print(f'All data shape: {all_data.shape}, All labels shape: {all_labels.shape}')
-    return all_data, all_labels
-
-def normalize_data(data):
-    # Normalize the data to have values between -0.5 and 0.5
-    data = data.astype('float32') / 255.0  # Scale pixel values to [0, 1]
-    data = data - 0.5  # Shift pixel values to [-0.5, 0.5]
-    return data
 
 def load_data():
-    # Load CIFAR-10 dataset
-    print('Loading CIFAR-10 dataset...')
-    cifar_data, cifar_labels = load_all_batches()
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # Normalize CIFAR-10 data
-    normalized_data = normalize_data(cifar_data)
-    print(f'Labels shape: {cifar_labels.shape}')
-    return normalized_data, cifar_labels
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return trainloader, testloader
+
+'''
+    def imshow(img):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+        
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+'''
--- main.py
+++ main.py
@@ -1,4 +1,61 @@
 from load_data import load_data
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class ThreeLayerCNN(nn.Module):
+    def __init__(self):
+        super(ThreeLayerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, 5)
+        self.conv2 = nn.Conv2d(24, 48, 3)
+        self.conv3 = nn.Conv2d(48, 96, 3)
+        self.fc1 = nn.Linear(96 * 2 * 2, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x), dim=1)
+        return x
 
 if __name__ == '__main__':
-    normalized_data, label = load_data()
+    train_loader, test_loader = load_data()
+    model = ThreeLayerCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+    PATH = './cifar_net.pth'
+    torch.save(model.state_dict(), PATH)
+
+    # model.load_state_dict(torch.load(PATH, weights_only=True))
+    # outputs = model(images)
