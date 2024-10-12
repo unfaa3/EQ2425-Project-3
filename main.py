8eb7eaa15a6f88f21a18d33a2dad298d4825cb1e
Jiacheng Xu <xujiacheng1016@hotmail.com>
2024-10-12 15:44:15 +0200

    ABCDE能跑了

--- main.py
+++ main.py
@@ -3,63 +3,112 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-
-class ThreeLayerCNN(nn.Module):
-    def __init__(self):
-        super(ThreeLayerCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 24, 5)
-        self.conv2 = nn.Conv2d(24, 48, 3)
-        self.conv3 = nn.Conv2d(48, 96, 3)
-        self.fc1 = nn.Linear(96 * 2 * 2, 512)
-        self.fc2 = nn.Linear(512, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2)
-        x = torch.flatten(x, 1)
-        x = F.relu(self.fc1(x))
-        x = F.softmax(self.fc2(x), dim=1)
-        return x
+from net import Net, Net_A1, Net_A2, Net_B, Net_C, Net_D, Net_E
 
 if __name__ == '__main__':
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
-    train_loader, test_loader = load_data()
-    model = ThreeLayerCNN()
+    net = Net()
+    net.to(device)
+    trainloader, testloader = load_data()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    def train_net(net, trainloader, optimizer, epochs=300):
+        net.train()
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data[0].to(device), data[1].to(device)
 
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+                optimizer.zero_grad()  # 使用传入的优化器
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()  # 使用传入的优化器进行更新
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+                running_loss += loss.item()
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
+            print(f'Epoch {epoch + 1}, Loss: {running_loss / len(trainloader)}')
+        print('Finished Training')
 
-    print('Finished Training')
-    PATH = './cifar_net.pth'
-    torch.save(model.state_dict(), PATH)
 
-    model.load_state_dict(torch.load(PATH, weights_only=True))
-    outputs = model(images)
+    def test_net(net, testloader):
+        net.eval()
+        num_classes = 10  # Assuming 10 classes
+        true_positives = [0] * num_classes
+        false_negatives = [0] * num_classes
+
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data[0].to(device), data[1].to(device)
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+
+                for cls in range(num_classes):
+                    true_positives[cls] += ((predicted == cls) & (labels == cls)).sum().item()
+                    false_negatives[cls] += ((predicted != cls) & (labels == cls)).sum().item()
+
+        recall_per_class = []
+        for cls in range(num_classes):
+            tp = true_positives[cls]
+            fn = false_negatives[cls]
+            if tp + fn > 0:
+                recall = tp / (tp + fn)
+            else:
+                recall = 0.0
+            recall_per_class.append(recall)
+            print(f'Recall of class {cls}: {recall * 100:.2f}%')
+
+        average_recall = sum(recall_per_class) / num_classes
+        print(f'Average Top-1 Recall Rate: {average_recall * 100:.2f}%')
+        return average_recall
+
+    optimizer = optim.SGD(net.parameters(), lr=1e-3)
+    print("Training Net...")
+    train_net(net, trainloader, optimizer)
+    print("Testing Net...")
+    recall = test_net(net, testloader)
+
+    net_a1 = Net_A1().to(device)
+    net_a2 = Net_A2().to(device)
+    optimizer_a1 = optim.SGD(net_a1.parameters(), lr=1e-3)
+    optimizer_a2 = optim.SGD(net_a2.parameters(), lr=1e-3)
+    # 训练模型A1
+    print("Training Net_A1...")
+    train_net(net_a1, trainloader, optimizer_a1)
+    print("Testing Net_A1...")
+    recall_a1 = test_net(net_a1, testloader)
+    print("Training Net_A2...")
+    train_net(net_a2, trainloader, optimizer_a2)
+    print("Testing Net_A2...")
+    recall_a2 = test_net(net_a2, testloader)
+
+    net_b = Net_B().to(device)
+    optimizer_b = optim.SGD(net_b.parameters(), lr=1e-3)
+    print("Training Net_B...")
+    train_net(net_b, trainloader, optimizer_b)
+    print("Testing Net_B...")
+    recall_b = test_net(net_b, testloader)
+
+    net_c = Net_C().to(device)
+    optimizer_c = optim.SGD(net_c.parameters(), lr=1e-3)
+    print("Training Net_C...")
+    train_net(net_c, trainloader, optimizer_c)
+    print("Testing Net_C...")
+    recall_c = test_net(net_c, testloader)
+
+    net_d = Net_D().to(device)
+    optimizer_d = optim.SGD(net_d.parameters(), lr=1e-3)
+    print("Training Net_D...")
+    train_net(net_d, trainloader, optimizer_d)
+    print("Testing Net_D...")
+    recall_d = test_net(net_d, testloader)
+
+    net_e = Net_E().to(device)
+    optimizer_e = optim.SGD(net_e.parameters(), lr=1e-3)
+    print("Training Net_E...")
+    train_net(net_e, trainloader, optimizer_e)
+    print("Testing Net_E...")
+    recall_e = test_net(net_e, testloader)
---
+++ net.py
@@ -0,0 +1,221 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(24, 48, kernel_size=3, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(48, 96, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(96 * 2 * 2, 512)
+        self.relu_fc1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 10)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 96 * 2 * 2)
+        x = self.relu_fc1(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class Net_A1(nn.Module):
+    def __init__(self):
+        super(Net_A1, self).__init__()
+        # 第一层卷积
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第三层卷积
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 全连接层
+        self.fc1 = nn.Linear(256 * 2 * 2, 512)
+        self.relu_fc1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 256 * 2 * 2)
+        x = self.relu_fc1(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class Net_A2(nn.Module):
+    def __init__(self):
+        super(Net_A2, self).__init__()
+        # 原始卷积层保持不变
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(24, 48, kernel_size=3, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(48, 96, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 全连接层
+        self.fc1 = nn.Linear(96 * 2 * 2, 512)
+        self.relu_fc1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 128)
+        self.relu_fc2 = nn.ReLU()
+        self.fc3 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 96 * 2 * 2)
+        x = self.relu_fc1(self.fc1(x))
+        x = self.relu_fc2(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class Net_B(nn.Module):
+    def __init__(self):
+        super(Net_B, self).__init__()
+        # 第一层卷积，滤波器大小7x7
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积，滤波器大小5x5
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第三层卷积保持不变
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 全连接层保持不变
+        self.fc1 = nn.Linear(256 * 1 * 1, 512)  # 需要根据输出大小调整
+        self.relu_fc1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 256 * 1 * 1)
+        x = self.relu_fc1(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class Net_C(nn.Module):
+    def __init__(self):
+        super(Net_C, self).__init__()
+        negative_slope = 0.01
+        # 卷积层和滤波器大小根据上一步的preferred_net
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=0)
+        self.lrelu1 = nn.LeakyReLU(negative_slope)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0)
+        self.lrelu2 = nn.LeakyReLU(negative_slope)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(256 * 1 * 1, 512)
+        self.lrelu_fc1 = nn.LeakyReLU(negative_slope)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.lrelu1(self.conv1(x)))
+        x = self.pool2(self.lrelu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 256 * 1 * 1)
+        x = self.lrelu_fc1(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class Net_D(nn.Module):
+    def __init__(self):
+        super(Net_D, self).__init__()
+        negative_slope = 0.01
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=0)
+        self.lrelu1 = nn.LeakyReLU(negative_slope)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0)
+        self.lrelu2 = nn.LeakyReLU(negative_slope)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(256 * 1 * 1, 512)
+        self.lrelu_fc1 = nn.LeakyReLU(negative_slope)
+        self.dropout = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.lrelu1(self.conv1(x)))
+        x = self.pool2(self.lrelu2(self.conv2(x)))
+        x = self.pool3(self.conv3(x))
+        x = x.view(-1, 256 * 1 * 1)
+        x = self.lrelu_fc1(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
+class Net_E(nn.Module):
+    def __init__(self):
+        super(Net_E, self).__init__()
+        negative_slope = 0.01
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=0)
+        self.lrelu1 = nn.LeakyReLU(negative_slope)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0)
+        self.lrelu2 = nn.LeakyReLU(negative_slope)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(256 * 1 * 1, 512)
+        self.lrelu_fc1 = nn.LeakyReLU(negative_slope)
+        self.bn_fc = nn.BatchNorm1d(512)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.pool1(self.bn1(self.lrelu1(self.conv1(x))))
+        x = self.pool2(self.bn2(self.lrelu2(self.conv2(x))))
+        x = self.pool3(self.bn3(self.conv3(x)))
+        x = x.view(-1, 256 * 1 * 1)
+        x = self.bn_fc(self.lrelu_fc1(self.fc1(x)))
+        x = self.fc2(x)
+        return x
